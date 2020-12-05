95c63c13f9851b01f1ad2801dc943eeba358881a
Ewan-desktop <chl8263@naver.com>
2020-12-05 15:47:12 +0900

    init

--- Scheduler task/Program.cs
+++ Scheduler task/Program.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Scheduler_task
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Scheduler_task{
+    class Program{
+        static void Main(string[] args){
+
         }
     }
 }
